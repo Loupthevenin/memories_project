cc475313fc4d1b7189a6eeaca1455eaf059305c9
MrShadowsWolf <loup.thevenin.1@gmail.com>
2024-01-19 13:14:25 +0100

    Server side

--- server/index.js
+++ server/index.js
@@ -3,19 +3,27 @@ import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
 
+import dotenv from 'dotenv';
 import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
 
 const app = express();
+dotenv.config();
 
 app.use(bodyParser.json({ limit: "30mb", extended : true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended : true }));
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use('/user', userRoutes);
+
+app.get('/', (req, res) => {
+    res.send('APP IS RUNNING.')
+});
 
 // https://www.mongodb.com/cloud/atlas
 
-const CONNECTION_URL = 'mongodb+srv://loupthevenin1:uN649uDNiX2qvzBZ@clustermemories.uwnje3o.mongodb.net/?retryWrites=true&w=majority'
+const CONNECTION_URL = 'URL'
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,17 @@
 import express from "express";
 
-import { getPosts, createPost, updatePost } from '../controllers/posts.js'
+import { getPostsBySearch, getPosts, getPost, createPost, updatePost, deletePost, likePost, commentPost } from '../controllers/posts.js';
+import auth from '../middleware/auth.js';
 
 const router = express.Router();
 
+router.get('/search', getPostsBySearch);
 router.get('/', getPosts);
-router.post('/', createPost);
-router.patch('/:id', updatePost)
+router.get('/:id', getPost);
+router.post('/', auth, createPost);
+router.patch('/:id', auth, updatePost);
+router.delete('/:id', auth, deletePost);
+router.patch('/:id/likePost', auth, likePost);
+router.post('/:id/commentPost', auth, commentPost);
 
 export default router;
